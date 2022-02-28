fd696acbe43282b28ec76418ba915b7c9b1e5a1c
MJH <97804686+MJH123123@users.noreply.github.com>
2022-02-28 10:25:02 -0500

    Create node_red_palettes.sh

---
+++ node_red_palettes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ #######################################
+ # Bash script to install NODE-RED palettes
+ # Author: jubair
+
+## node-red-node-mysql 1.0.1
+npm install node-red-node-mysql
+
+## node-red-contrib-cip-ethernet-ip 1.1.3
+npm install node-red-contrib-cip-ethernet-ip
+
+## node-red-node-ui-table 0.3.12
+npm install node-red-node-ui-table
+
+## node-red-node-random 0.4.0
+npm install node-red-node-random
+
+## node-red-contrib-modbus 5.21.2
+npm install node-red-contrib-modbus
+
+## dashboard-evi 1.0.2
+npm install dashboard-evi
+
+## node-red-dashboard 3.1.6
+## npm install node-red-dashboard
+
+
