fd84eba2b3439fda8ebf39c9e61a5b89b90e077a
MJH <97804686+MJH123123@users.noreply.github.com>
2022-02-28 10:23:31 -0500

    Create node_red_pm2.sh

---
+++ node_red_pm2.sh
@@ -0,0 +1,26 @@
+ #!/bin/bash
+
+ #######################################
+ # Bash script to install NODE-RED and PM2 stack in ubuntu
+ # Author: jubair
+
+## system update & upgrade
+apt-get update -y
+apt-get upgrade -y
+
+## Install NPM
+apt-get install npm -y
+
+## Install Node-Red
+npm install -g --unsafe-perm node-red
+
+## Install PM2
+npm install -g pm2 -y
+
+# Tell PM2 to run Node-RED
+pm2 start /usr/local/bin/node-red -- -v
+
+# Tell PM2 to run on boot
+pm2 save
+pm2 startup
+
